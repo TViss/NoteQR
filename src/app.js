e81046793c29ae68cd521b06c506d526321f88c6
Lane Viss <lviss@users.noreply.github.com>
2017-02-20 10:47:55 -0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,47 @@
+var angular = require('angular');
+var app = angular.module("qrcode", [require('angular-route'), require('angular-material')]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "templates/main.html",
+	controller : function($scope, $http)
+	{
+		$scope.qrcodes=[];
+		$http.get("/qrcodes").then(function(res)
+		{
+			$scope.qrcodes = $scope.qrcodes.concat(res.data);	
+		});
+		$scope.genqr=function()
+		{
+			$http.get("/generate").then(function(res)
+			{
+				$scope.qrcodes.push(res.data);	
+			});
+		};	
+	}
+    })
+    .when("/message/:hash", {
+        templateUrl : "templates/message.html",
+	controller : function($scope, $http, $routeParams)
+	{
+		$scope.qrcode = {};
+		$http.get("/qrcode/" + $routeParams.hash).then(function(res)
+		{
+			$scope.qrcode = res.data;	
+		});
+		$scope.save=function()
+		{
+			$http.put("/qrcode/" + $routeParams.hash, $scope.qrcode).then(function(res)
+			{
+				alert("Saved Successfully");
+			});
+		};	
+	}
+    })
+    .when("/green", {
+        templateUrl : "green.htm"
+    })
+    .when("/blue", {
+        templateUrl : "blue.htm"
+    });
+});
